2f6ea94c57b10796e62ebd8a373c0a51e1168ef8
Ji Yim <gsooyim@gmail.com>
2018-04-10 22:29:32 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string="Squawk!")
-  puts string
-  string
+def parrot(input="Squawk!")
+  puts input
+  input
 end
